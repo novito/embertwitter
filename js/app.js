afe247f2ffedb0e07e2144eca433a312a99b4b50
Joan Roig <roigarderiu@gmail.com>
2012-08-09 19:54:54 -0700

    Changed App name

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-var App = Em.Application.create();
+var EmberTwitter = Em.Application.create({
+  ready: function(){
+    alert('Launching the EmberTwitter App!');
+  }
+});
 /* Models */
-App.store = DS.Store.create({
-  revision: 4
+EmberTwitter.store = DS.Store.create({
+  revision: 4,
+  adapter: DS.RESTAdapter.create({ bulkCommit: false })
 });
 
 
 /* Views */
-App.MyView = Em.View.extend({
+EmberTwitter.MyView = Em.View.extend({
   mouseDown: function() {
-    window.alert("hello world!");
+    window.alert("Ember Twitter App!");
   }
 });
